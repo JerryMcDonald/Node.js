bff45c5ea2e658a83ace6999400fd68ac512fdbe
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-01-07 20:19:10 -0600

    new app.js with additional files

--- 1-Node-Project/routes.js
+++ 1-Node-Project/routes.js
@@ -22,7 +22,7 @@ const requestHandler = (req, res) => {
 
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
+      const message = parsedBody.split('=')[0];
       console.log(message);
       fs.writeFile('message.txt', message, (error) => {
         res.statusCode = 302;
@@ -31,6 +31,7 @@ const requestHandler = (req, res) => {
       });
     });
   }
+
 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
