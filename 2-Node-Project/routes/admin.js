b6db47b6b5092fad2fd7136f1e1eb2ed68ebf2b2
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-01-07 20:20:47 -0600

    admin.js

---
+++ 2-Node-Project/routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  // the default res header is text/html
+  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+});
+
+// /admin/add-product => POST
+router.post('/add-product',(req, res, next) => {
+  console.log(req.body.title);
+  res.redirect('/');
+})
+
+module.exports = router;
