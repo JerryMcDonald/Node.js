0a68a6a7c834498fef3b6dc1d65b473f17aed85d
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-01-07 20:18:50 -0600

    remove app.js

--- 1-Node-Project/app.js
+++
@@ -1,9 +0,0 @@
-//core modules
-const http = require('http');
-
-//custom files
-const {someText, handler} = require('./routes');
-
-const server = http.createServer(handler);
-
-server.listen(3000);
