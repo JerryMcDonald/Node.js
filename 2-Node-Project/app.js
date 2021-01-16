ccc9dd5dedc6def3790bf7e2a141c3c07543aba3
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-01-16 12:56:02 -0600

    the default directory for the view engine is views - so we do not need to specify

--- 2-Node-Project/app.js
+++ 2-Node-Project/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+// app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
