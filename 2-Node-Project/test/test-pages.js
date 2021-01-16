46edec0bac42acad10e1193f4369b908a64c4cfa
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-01-16 12:55:29 -0600

    more specific test page

--- 2-Node-Project/test/test-pages.js
+++ 2-Node-Project/test/test-pages.js
@@ -1,9 +1,17 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
-it('status', function(done) {
-    request('http://localhost:3000/sdfsdf' , function(error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
-    });
+
+describe('#PageTests()', function() {
+
+    context('Error Page', function() {
+        it('status', function(done) {
+            request('http://localhost:3000/sdfsdf' , function(error, response, body) {
+                console.log('response', response);
+                expect(response.statusCode).to.equal(404);
+                done();
+            });
+        });
+    })
+
 });
